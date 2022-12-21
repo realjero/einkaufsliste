3b2b6ef39ae869c405c85b93e2a5a8b15b3f5ca8
jerome <jerome_jero@t-online.de>
2022-12-21 10:52:14 +0100

    update

--- scripts/client.ts
+++ scripts/client.ts
@@ -1,16 +1,132 @@
+let editing = false;
+
 function loadList() {
     const req = new XMLHttpRequest();
-    req.open('GET', '/get', true);
+    req.open('GET', 'get');
     req.send();
     req.onload = () => {
         if(req.status == 200) {
             document.getElementById('table_items').innerHTML = req.response;
         } else {
-            console.log("loadList error")
+            console.log("loadList error");
+        }
+    };
+}
+
+function newEntry(name: string) {
+    const js = {
+        "name": name
+    };
+    const req = new XMLHttpRequest();
+    req.open('POST', '/new');
+    req.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+    req.send(JSON.stringify(js));
+    req.onload = () => {
+        if(req.status === 200) {
+            loadList();
+        } else {
+            console.log("new entry: " + req.status);
+        }
+    };
+}
+
+function save() {
+    const req = new XMLHttpRequest();
+    req.open('GET', 'save');
+    req.send();
+    req.onload = () => {
+        if(req.status === 200) {
+            loadList();
+        } else {
+            console.log("save: " + req.status);
+        }
+    };
+}
+
+function show_button(event) {
+    event.preventDefault();
+    if(!editing) {
+        if(event.target.parentElement.tagName === 'TR') {
+            editing = true;
+            const index = event.target.parentElement.rowIndex;
+            document.getElementById("button_holder" + index).innerHTML = buttons;
+            document.getElementById("table_element" + index + "0").contentEditable = 'true';
+            document.getElementById("table_element" + index + "1").contentEditable = 'true';
+            document.getElementById("table_element" + index + "2").contentEditable = 'true';
+            document.getElementById("table_element" + index + "3").contentEditable = 'true';
+            event.target.focus();
         }
     }
 }
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    loadList();
+function updateBackDelete(event) {
+    if(editing) {
+        if (event.target.id === 'back') {
+            editing = false;
+            const index = event.target.parentElement.parentElement.rowIndex;
+            document.getElementById("button_holder" + index).innerHTML = "";
+            document.getElementById("table_element" + index + "0").contentEditable = 'false';
+            document.getElementById("table_element" + index + "1").contentEditable = 'false';
+            document.getElementById("table_element" + index + "2").contentEditable = 'false';
+            document.getElementById("table_element" + index + "3").contentEditable = 'false';
+            loadList();
+        } else if (event.target.id === 'update') {
+            editing = false;
+            const index = event.target.parentElement.parentElement.rowIndex;
+            const request = new XMLHttpRequest();
+            const js = {
+                "id": (index - 1),
+                "wer": document.getElementById("table_element" + index + "0").innerHTML,
+                "was": document.getElementById("table_element" + index + "1").innerHTML,
+                "wo": document.getElementById("table_element" + index + "2").innerHTML,
+                "wann": document.getElementById("table_element" + index + "3").innerHTML
+            };
+            request.open('POST', 'update');
+            request.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+            request.send(JSON.stringify(js));
+            request.onload = () => {
+                if(request.status === 200) {
+                    loadList();
+                } else {
+                    console.log("update: " + request.status);
+                }
+            }
+        } else if (event.target.id === 'delete') {
+            editing = false;
+            const request = new XMLHttpRequest();
+            request.open('POST', 'remove');
+            request.send(JSON.stringify({"id": event.target.parentElement.parentElement.rowIndex}));
+            request.onload = () => {
+                if(request.status === 200) {
+                    loadList();
+                } else {
+                    console.log("delete: " + request.status);
+                }
+            }
+        }
+    }
+}
+
+const buttons = '<button type="button" class="editable" id="update">U</button>' +
+    '<button type="button" class="editable" id="back">B</button>' +
+    '<button type="button" class="editable" id="delete">X</button>';
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById("new").addEventListener('click', (event) => {
+        if(!editing) {
+            const user_input = document.getElementById("user") as HTMLInputElement;
+            newEntry(user_input.value);
+        }
+    });
+
+    document.getElementById("save").addEventListener('click', (event) => {
+        if(!editing) {
+            save();
+        }
+    });
+
+    document.getElementById("table_items").addEventListener('click', (event) => {
+        show_button(event);
+        updateBackDelete(event);
+    });
 })
--- server.ts
+++ server.ts
@@ -8,11 +8,36 @@ class Eintrag {
     public wo: string;
     public wann: Date;
 
-    constructor(wer: string, was: string, wo: string, wann: Date) {
+    constructor(wer: string, was: string, wo: string, wann: string) {
         this.wer = wer;
         this.was = was;
         this.wo = wo;
-        this.wann = wann;
+        //this.wann = wann;
+    }
+
+    getDate(): string {
+        var day = new Date(this.wann);
+        console.log(day === !isNaN());
+        if(this.wann === null) {
+            return "";
+        }
+        return [day.getDate(),
+                day.getMonth()+1,
+                day.getFullYear()].join('.')+' '+
+            [day.getHours(),
+                day.getMinutes()].join(':');
+    }
+
+    setDate(d: string) {
+        console.log(d)
+        let datetime = d.split(".");
+        let date = new Date();
+        date.setDate(Number(datetime[0]));
+        date.setMonth(Number(datetime[1]));
+        date.setFullYear(Number(datetime[2]));
+        date.setHours(Number(datetime[3]));
+        date.setMinutes(Number(datetime[4]));
+        this.wann = date;
     }
 }
 
@@ -26,7 +51,7 @@ class Einkaufsliste {
         return this.list;
     }
 
-    addItem(wer: string, was: string, wo: string, wann: Date): void {
+    addItem(wer: string, was: string, wo: string, wann: string): void {
         this.list.push(new Eintrag(wer, was, wo, wann));
     }
 
@@ -38,11 +63,12 @@ class Einkaufsliste {
         this.list[n].wer = u.wer;
         this.list[n].was = u.was;
         this.list[n].wo = u.wo;
-        this.list[n].wo = u.wo;
+        this.list[n].wann = u.wann;
     }
 
     getHTML(): string {
         let html: string;
+        let index: number = 0;
         html = '<tr>' +
             '<th>Wer</th>' +
             '<th>Was</th>' +
@@ -52,23 +78,25 @@ class Einkaufsliste {
             '</tr>'
         this.list.forEach(x => {
             html = html + '<tr>' +
-                '<td contenteditable>' + x.wer + '</td>' +
-                '<td contenteditable>' + x.was + '</td>' +
-                '<td contenteditable>' + x.wo + '</td>' +
-                '<td contenteditable>' + x.wann + '</td>' +
-                '<td></td>' +
-                '</tr>'
+                '<td id="table_element' + (index + 1) + '0" contenteditable="false">' + x.wer + '</td>' +
+                '<td id="table_element' + (index + 1) + '1" contenteditable="false">' + x.was + '</td>' +
+                '<td id="table_element' + (index + 1) + '2" contenteditable="false">' + x.wo + '</td>' +
+                '<td id="table_element' + (index + 1) + '3" contenteditable="false">' + x.getDate() + '</td>' +
+                '<td id="button_holder' + (index + 1) + '"></td>' +
+                '</tr>';
+            index++;
         });
         return html;
     }
 
     saveJSON(): void {
        fs.writeFile("data.json", JSON.stringify(this.list), (err) => {
-           if(err) console.log("Cannot Save JSON")
+           if(err) console.log("Cannot Save JSON");
        });
     }
 
     loadJSON(): void {
+        this.list = [];
         let data = JSON.parse(fs.readFileSync("data.json"));
         for(let i = 0; i < data.length; i++) {
             this.addItem(data[i].wer, data[i].was, data[i].wo, data[i].wann);
@@ -80,8 +108,6 @@ class Einkaufsliste {
 
 const ek = new Einkaufsliste();
 
-ek.loadJSON();
-
 server.use("/scripts", express.static(__dirname + "/scripts"));
 server.use("/styles", express.static(__dirname + "/styles"));
 server.use(express.json());
@@ -92,8 +118,9 @@ server.get('/', (req, res) => {
 });
 
 server.post('/new', (req, res) => {
-    ek.addItem(req.body.name, "", "", new Date(Date.now()));
-
+    ek.loadJSON();
+    ek.addItem(req.body.name, "", "", "");
+    ek.saveJSON();
     res.status(200);
     res.send("Successfully added: " + req.body.name);
 });
@@ -105,19 +132,23 @@ server.get('/save', (req, res) => {
 });
 
 server.post('/update', (req, res) => {
-    const eintrag = new Eintrag(req.body.wer, req.body.was, req.body.wo, req.body.wann);
+    const eintrag = new Eintrag(req.body.wer, req.body.was, req.body.wo, "");
+    eintrag.setDate(req.body.wann);
     ek.updateItem(req.body.id, eintrag);
+    ek.saveJSON();
     res.status(200);
     res.send("Successfully updated");
 });
 
 server.post('/remove', (req, res) => {
     ek.removeItem(req.body.id);
+    ek.saveJSON();
     res.status(200);
     res.send("Successfully removed");
 });
 
 server.get('/get', (req, res) => {
+    ek.loadJSON();
     res.status(200);
     res.send(ek.getHTML());
 });
